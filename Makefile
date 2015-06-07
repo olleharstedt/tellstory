dc5df213ea2e7480355999019add84df32a14e49
Olle Harstedt <olleharstedt@yahoo.com>
2015-06-07 15:36:30 +0200

    Added tellstory cgi file.

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-INCLUDE					:= -package xml-light,str,pcre,core,bolt,ppx_deriving,ppx_deriving.show -linkpkg -thread -g
+INCLUDE					:= -package xml-light,str,pcre,core,bolt,ppx_deriving,ppx_deriving.show,netcgi2 -linkpkg -thread -g
 PGFLAGS         := --infer
 GENERATED       := parser.ml parser.mli lexer.ml
-MODULES         := ast globals parser lexer tparser tlexer tellstory main
-EXECUTABLE      := tellstory
+#MODULES         := ast globals parser lexer tparser tlexer tellstory main
+MODULES         := ast globals parser lexer tparser tlexer tellstory tellstory_cgi
+EXECUTABLE      := tellstory.cgi
 OCAMLDEPWRAPPER := ocamldep.wrapper
 include Makefile.shared
 include Makefile.auto
---
+++ tellstory_cgi.ml
@@ -0,0 +1,31 @@
+(**
+ * CGI-file for tellstory
+ *
+ * @since 2015-06-07
+ * @author Olle Härstedt
+ *)
+
+open Printf
+
+(* Create an HTML escaping function for the UTF-8 encoding. *)
+let escape_html = Netencoding.Html.encode ~in_enc:`Enc_utf8 ()
+
+let process (cgi : Netcgi.cgi) =
+	cgi#set_header
+		~cache:`No_cache
+		~content_type:"text/html; charset=\"utf-8\"\r\n\r\n"
+		();
+
+  printf "<!DOCTYPE html><html><head></head><body><p>Hej</p></body></html>";
+
+	(* Flush the output buffer. *)
+	cgi#out_channel#commit_work();
+
+  cgi#finalize()
+
+
+let _ =
+  let config = Netcgi.default_config in
+  let buffered _ ch = new Netchannels.buffered_trans_channel ch in
+  (*Netcgi_cgi.run ~config:config ~output_type:(`Transactional buffered) process*)
+  Netcgi_test.run ~config:config ~output_type:(`Transactional buffered) process
