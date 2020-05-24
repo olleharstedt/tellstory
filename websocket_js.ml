a3840136c8d0acb87c9296d7d746a0577e696843
Olle <noemail>
2020-05-24 23:29:32 +0000

    Translate websocket JS to js_of_ocaml

--- websocket_js.ml
+++ websocket_js.ml
@@ -4,5 +4,24 @@
  *   js_of_ocaml websocket_js.byte
  *)
 open Js_of_ocaml
+open Js
 
-let console = Js.Unsafe.global##console
+let _ =
+  Firebug.console##log (string "here");
+  let ws : WebSockets.webSocket t = new%js WebSockets.webSocket (string "ws://localhost:8080") in
+  (** TODO: Fill in the blanks, Dom.event ect *)
+  let onopen : ('a t, 'a Dom.event t) Dom.event_listener = Dom.handler
+    (fun s ->
+      ws##send (string "asd");
+      bool false
+    )
+  in
+  let onmessage : (_, _) Dom.event_listener = Dom.handler
+    (fun ev ->
+      Firebug.console##log (string ("Message from server: " ^ (to_string ev##.data)));
+      bool false
+    )
+  in
+  ws##.onopen    := onopen;
+  ws##.onmessage := onmessage;
+  ()
