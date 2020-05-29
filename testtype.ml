93c72a5b1e6590ce0a3429d0ed3d9fefdc91b076
Olle <noemail>
2020-05-29 20:14:57 +0000

    Test some more serialization

--- testtype.ml
+++ testtype.ml
@@ -1,24 +1,33 @@
-type t = One | Two of int | Three of string
+type test = One | Two of int | Three of string
+[@@deriving json]
+type fest = One | Two of int | Three of float
 [@@deriving json]
 
 (**
  * ocamlfind opt -package js_of_ocaml-ppx_deriving_json -linkpkg testtype.ml
  *)
 let _ =
-  let value : t = Three "foo" in
+  let value : test = Two 2 in
   let buffer : Buffer.t = Buffer.create 255 in
-  to_json buffer value;
+  test_to_json buffer value;
   let content : string = Buffer.contents buffer in
   print_endline content;
+
+  let value : fest = Two 2 in
+  let buffer : Buffer.t = Buffer.create 255 in
+  fest_to_json buffer value;
+  let content2 : string = Buffer.contents buffer in
+  print_endline content2;
+
   (*
   let buf : Lexing.lexbuf = Lexing.from_string (Buffer.contents buffer) in
   let value : t = of_json (Deriving_Json_lexer.init_lexer buf) in
   *)
-  let value : t =
-       content
+  let value : test =
+       content2
     |> Lexing.from_string
     |> Deriving_Json_lexer.init_lexer
-    |> of_json
+    |> test_of_json
   in
   match value with
   | One -> print_endline "One!"
